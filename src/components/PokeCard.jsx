a4b82ca0ba008c422de2a3507d7220a4f6e70a83
yewon97 <dpdnjs9936@gmail.com>
2024-05-13 22:38:37 +0900

    포켓몬 카드 꾸미기

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -13,7 +13,7 @@ export default function PokeCard({ url, name }) {
   async function fetchPokemonDetailData() {
     try {
       const response = await axios.get(url);
-      console.log("response: ", response.data);
+
       const pokemonData = formatPokemonData(response.data);
       setPokemon(pokemonData);
     } catch (error) {
@@ -31,5 +31,36 @@ export default function PokeCard({ url, name }) {
     return PokeData;
   }
 
-  return <div>PokeCard</div>;
+  const bg = `bg-${pokemon?.type}`;
+  const border = `border-${pokemon?.type}`;
+  const text = `text-${pokemon?.type}`;
+
+  const img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon?.id}.png`;
+
+  return (
+    <>
+      {pokemon && (
+        <a
+          href={`/pokemon/${name}`}
+          className={`box-border rounded-lg ${border} w-[8.5rem] h-[8.5rem] z-0 bg-slate-800 justify-between items-center`}
+        >
+          <div
+            className={`${text} h-[1.5rem] text-xs w-full pt-1 px-2 text-right rounded-t-lg`}
+          >
+            #{pokemon?.id.toString().padStart(3, "00")}
+          </div>
+          <div className={`w-full f-6 flex item-center justify-center`}>
+            <div className="box-border relative flex w-full h-[5.5rem] basic justify-center items-center">
+              <img src={img} alt={name} className={`object-contain h-full`} />
+            </div>
+          </div>
+          <div
+            className={`${bg} text-xs text-zinc-100 h-[1.5rem] rounded-b-lg uppercase font-medium pt-1`}
+          >
+            {pokemon.name}
+          </div>
+        </a>
+      )}
+    </>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
     "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
     "./src/styles/main.css",
   ],
+  safelist: [
+    {
+      pattern: /^text-|^bg-/, // You can display all the colors that you need
+    },
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#ffcb05",
+        second: "#3d7dca",
+        third: "#003a70",
+        normal: "#A8A77A",
+        fire: "#EE8130",
+        water: "#6390F0",
+        electric: "#dfbc30",
+        grass: "#7AC74C",
+        ice: "#97d4d2",
+        fighting: "#b83e3a",
+        poison: "#A33EA1",
+        ground: "#E2BF65",
+        flying: "#A98FF3",
+        psychic: "#F95587",
+        bug: "#A6B91A",
+        rock: "#B6A136",
+        ghost: "#735797",
+        dragon: "#6F35FC",
+        dark: "#705746",
+        steel: "#B7B7CE",
+        fairy: "#D685AD",
+        none: "#bfbfbf",
+      },
+    },
   },
   plugins: [],
 };
