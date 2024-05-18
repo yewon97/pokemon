2e90553bed0f8f6d5792972bbb24ac8030647403
yewon97 <dpdnjs9936@gmail.com>
2024-05-19 00:21:16 +0900

    search input, debounce 생성

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,56 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
 import PokeCard from "./components/PokeCard";
+import { useDebounce } from "./hooks/useDebounce";
 
 function App() {
   const [pokemons, setPokemons] = useState([]);
+
+  const [offset, setOffset] = useState(0);
+  const [limit, setLimit] = useState(20);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const debouncedSearchTerm = useDebounce(searchTerm, 500); // 0.5초
+
   useEffect(() => {
-    fetchPokeData();
+    fetchPokeData(true);
   }, []);
 
-  // 0 ~ 1008
-  const URL = "https://pokeapi.co/api/v2/pokemon/?limit=10&offset=0";
+  useEffect(() => {
+    handleSearchInput(debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
 
-  const fetchPokeData = async () => {
+  const handleSearchInput = async (serchTerm) => {
+    if (serchTerm.length > 0) {
+      try {
+        const response = await axios.get(
+          `https://pokeapi.co/api/v2/pokemon/${searchTerm}`,
+        );
+        const pokemonData = {
+          url: `https://pokeapi.co/api/v2/pokemon/${response.data.id}`,
+
+          name: searchTerm,
+        };
+        setPokemons([pokemonData]);
+      } catch (error) {
+        setPokemons([]);
+        console.error(error);
+      }
+    } else {
+      fetchPokeData(true);
+    }
+  };
+
+  const fetchPokeData = async (isFirstFetch) => {
     try {
+      const offsetValue = isFirstFetch ? 0 : offset + limit;
+      const URL = `https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offsetValue}`;
       const response = await axios
         .get(URL)
         .then((response) => response.data.results);
 
-      setPokemons(response);
+      setPokemons([...pokemons, ...response]);
+      setOffset(offsetValue);
     } catch (error) {
       console.log(error);
     }
@@ -27,19 +60,44 @@ function App() {
   return (
     <article className="pt-6">
       <header className="flex flex-col gap-2 w-full px-4 z-50">
-        Input form
+        <div className="relative z-50">
+          <form className="relative flex justify-center items-center w-[20.5rem] h-6 rounded-lg m-auto">
+            <input
+              type="text"
+              className="text-xs w-[20.5rem] h-6 rounded-lg px-2 p y-1 text-gray-300 text-center bg-[#687687]"
+              placeholder="포켓몬을 검색해주세요."
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <button
+              type="submit"
+              className="fa fa-2x text-xs bg-slate-900 text-slate-300 w-[2.5rem] h-6 px-2 py-1 rounded-r-lg text-center absolute right-0 hover:bg-slate-700 z-0"
+            >
+              검색
+            </button>
+          </form>
+        </div>
       </header>
-      <section className="pt-6 flex flex-col justify-center items-center overflow-auto z-0"></section>
-      <div className="flex flex-row flex-wrap gap-[16px] items-center justify-center px-2 max-w-4xl">
-        {pokemons.length > 0 ? (
-          pokemons.map(({ url, name }) => (
-            <PokeCard key={url} name={name} url={url} />
-          ))
-        ) : (
-          <h2 className="font-medium text-lg text-slate-900 mb-1">
-            포켓몬이 없습니다.
-          </h2>
-        )}
+      <section className="pt-6 flex flex-col justify-center items-center overflow-auto z-0">
+        <div className="flex flex-row flex-wrap gap-[16px] items-center justify-center px-2 max-w-4xl">
+          {pokemons.length > 0 ? (
+            pokemons.map(({ url, name }) => (
+              <PokeCard key={url} name={name} url={url} />
+            ))
+          ) : (
+            <h2 className="font-medium text-lg text-slate-900 mb-1">
+              포켓몬이 없습니다.
+            </h2>
+          )}
+        </div>
+      </section>
+      <div className="text-center">
+        <button
+          onClick={() => fetchPokeData(false)}
+          className="bg-slate-800 px-6 py-2 my-4 text-base rounded-lg font-bold text-white"
+        >
+          더보기
+        </button>
       </div>
     </article>
   );
